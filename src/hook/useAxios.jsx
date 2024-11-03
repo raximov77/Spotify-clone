d6c494d66b2837a14d7228236deab74f39e8661c
raximov77 <raximovikrom68@gmail.com>
2024-11-03 10:02:45 +0500

    /

---
+++ src/hook/useAxios.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function useAxios() {
+  return (
+    <div>useAxios</div>
+  )
+}
+
+export default useAxios
---
+++ src/hook/useEnv.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function useEnv() {
+  return (
+    <div>useEnv</div>
+  )
+}
+
+export default useEnv
