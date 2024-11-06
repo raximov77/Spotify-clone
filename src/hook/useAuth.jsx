4d54055260e4cede1d8da4be595150adae61d234
raximov77 <raximovikrom68@gmail.com>
2024-11-06 13:41:33 +0500

    /

--- src/hook/useAuth.jsx
+++ src/hook/useAuth.jsx
@@ -2,14 +2,14 @@ import { useEffect, useState } from 'react'
 import {useAxios} from "./useAxios"
 
 function useAuth(code) {
-  const [accsessToken, setAccsessToken] = useState(null)
+  const [accessToken, setAccessToken] = useState(null)
   useEffect(() => {
     useAxios().post("/login", {code}).then(res => {
-      setAccsessToken(res.data.accsessToken);
+      setAccessToken(res.data.accessToken);
       window.history.pushState({}, null, "/")
     }).catch(err => window.location = "/")
   }, [code])
-  return accsessToken
+  return accessToken
 }
 
 export default useAuth
